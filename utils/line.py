87b040d4f52eec25080e1615345c0491afa71ffe
IkezawaYuki <y.ikezawa93@gmail.com>
2024-02-05 23:21:54 +0900

    mod

---
+++ utils/line.py
@@ -0,0 +1,18 @@
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
+def execute_line():
+    # lineの送り主とメッセージを取得する
+
+    # cosmosdbを呼び出し、会話履歴を取得する
+
+    # azure ai search で回答の単語を検索する
+
+    # プロンプトを作成する
+
+    # azure openaiで処理を投げ、responseを受け取る
+
+    # lineで返答する。
+    pass
